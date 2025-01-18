f225d7fed20bb0d9f40ce0057a42391e72b40af8
Shweta Bharti <shwetabharti243@gmail.com>
2025-01-19 02:10:13 +0530

    Added database configuration and created model class
    Added database configuration and created model class

---
+++ student-management-system-backend/src/main/java/com/example/student_management_system/model/Student.java
@@ -0,0 +1,64 @@
+package com.example.student_management_system.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String age;
+
+    @Column(nullable = false)
+    private String studentClass;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getStudentClass() {
+		return studentClass;
+	}
+
+	public void setStudentClass(String studentClass) {
+		this.studentClass = studentClass;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+    
+}
